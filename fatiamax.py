3c7a1c0a423881a63ea5aa9b083e1bc3276bb492
GumemuraUb <gumemura123@gmail.com>
2019-10-15 19:56:10 -0300

    epx2 mas tem coisa errada

--- fatiamax.py
+++ fatiamax.py
@@ -56,31 +56,50 @@ def fatia_max(p, r, v):
         v[i:j] com p <= i <= j <= r.
     '''
     '''def numeros_consecutivos(lista, a = 0):
-        if (a + 1) < len(lista):
-            if lista[a] + 1 == lista[a + 1]:
-                numeros_consecutivos(lista, a + 1)
-            else:
-                return a
+        if (a + 1) < len(lista) and lista[a] + 1 == lista[a + 1]:
+            return numeros_consecutivos(lista, a + 1)
         else:
-            return None
+            return a
 
-    
-        itens_negativos = []
-        segmentos_positivos = []
-    
-        for elem in v:
-            if elem < 0:
-                itens_negativos.append(v.index(elem))
-    
-        if len(itens_negativos) == 0:
-            i = p
-            j = r
-            soma_max = sum(v[i,j])
+    itens_negativos = []
+    segmentos_positivos = []
+
+    for elem in v:
+        if elem < 0:
+            itens_negativos.append(v.index(elem))
+
+    if len(itens_negativos) == 0:
+        i = p
+        j = r
+        soma_max = sum(v[i,j])
+    else:
+        if itens_negativos[0] == p:
+
+    return soma_max, i, j'''
+
+    soma_temp = 0
+    soma_max = 0
+    pega_indice = True
+    a = p
+    a_temp = 0
+    j = r
+
+    for i in range(p, r):
+        if v[i] > 0:
+            soma_temp += v[i]
+            if pega_indice:
+                pega_indice = False
+                a_temp = i
         else:
-            if itens_negativos[0] == p:
+            if soma_temp > soma_max:
+                a = a_temp
+                j = i
+                soma_max = soma_temp
 
+            pega_indice = True
+            soma_temp = 0
 
-        return soma_max, i, j'''
+    return soma_max, a, j
 
 #--------------------------------------------------------
 def fatia_max_meio(p, q, r, v):
@@ -109,25 +128,16 @@ def fatia_max_div_conq(p, r, v):
     print("Vixe! Ainda não fiz a função fatia_max_div_conq()...")
     return -1, -1, -1
 
-
 def main():
-    def numeros_consecutivos(lista, a = 0):
-        if (a + 1) < len(lista):
-            if lista[a] + 1 == lista[a + 1]:
-                return numeros_consecutivos(lista, a + 1)
-            else:
-                return a
-        else:
-            return None
+    v = [31, -41, 59, 26, -53, 58, 97, -93, -23, 84]
 
-    print(numeros_consecutivos([5,6,7,4,2,3]), 5)
+    print(v)
+    print("\nEsperado: (187, 2, 7)\nResultado: ",fatia_max(0,10,v))
+
+    print("\nEsperado: (187, 2, 7)\nResultado: ",fatia_max(2,9,v))
+
+    print("\nEsperado: (155, 5, 7)\nResultado: ",fatia_max(3,9,v))
+
+    print("\nEsperado: (84, 9, 10)\nResultado: ",fatia_max(7,10,v))
 
 main()
-
-
-
-
-
-
-
-
